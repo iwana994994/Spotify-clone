d8b83e18e5c81a4a5a168c927fd869d160492458
iwana994994 <iwana994994@gmail.com>
2025-07-20 18:22:58 +0200

    logo

--- frontend/src/pages/admin/components/Header.tsx
+++ frontend/src/pages/admin/components/Header.tsx
@@ -1,8 +1,22 @@
+import HomePage from "@/pages/HomePage"
+import { UserButton } from "@clerk/clerk-react"
+import { Link } from "react-router-dom"
 
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="flex items-center justify-between p-4">
+      <div>
+        <Link to="/" className="text-2xl font-bold items-start">
+           <img src="../../../../public/spotify-cat-logo-512x512.png" alt="Logo" className="w-10 h-10 ml-2" />
+           <h1>Spotify</h1>
+           </Link>
+     
+
+      </div>
+      <h1 className="text-2xl font-bold">Admin Dashbord</h1>
+      <UserButton/>
+    </div>
   )
 }
 
