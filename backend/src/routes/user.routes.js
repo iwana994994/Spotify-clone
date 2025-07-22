c36a65f174a71c77b81ccf2c168c690399e79a96
iwana994994 <iwana994994@gmail.com>
2025-07-22 15:56:04 +0200

    error fixed on user.router.js

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -5,7 +5,7 @@ import { getAllUsers,getAllMessages } from "../controller/user.controller.js";
 const router = Router();    
 
 router.get("/",protectedRoute, getAllUsers);
-router.get("messages/:id",protectedRoute, getAllMessages);
+router.get("/messages/:id",protectedRoute, getAllMessages);
 
 
 export default router;
